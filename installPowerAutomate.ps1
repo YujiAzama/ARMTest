9ac7b38f23f3632cbf6259bc83001a7fc8ed9854
Yuji Azama <yuji.azama@gmail.com>
2022-05-10 21:55:50 +0900

    fix script

--- installPowerAutomate.ps1
+++ installPowerAutomate.ps1
@@ -1,2 +1,2 @@
 Invoke-WebRequest "https://go.microsoft.com/fwlink/?linkid=2102613" -OutFile Setup.Microsoft.PowerAutomate.exe
-Setup.Microsoft.PowerAutomate.exe -Silent -Install -ACCEPTEULA
+.\Setup.Microsoft.PowerAutomate.exe -Silent -Install -ACCEPTEULA
