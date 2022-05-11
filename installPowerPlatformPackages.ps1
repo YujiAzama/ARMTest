4f6789f990c44a31598acb8b8ab476885e889aa5
Yuji Azama <yuji.azama@gmail.com>
2022-05-11 11:53:10 +0900

    Add WindowsUpdate

--- installPowerPlatformPackages.ps1
+++ installPowerPlatformPackages.ps1
@@ -3,3 +3,8 @@ Invoke-WebRequest "https://go.microsoft.com/fwlink/?linkid=2102613" -OutFile Set
 
 Invoke-WebRequest "https://download.microsoft.com/download/8/8/0/880BCA75-79DD-466A-927D-1ABF1F5454B0/PBIDesktopSetup_x64.exe" -OutFile PBIDesktopSetup_x64.exe
 .\PBIDesktopSetup_x64.exe -silent -norestart ACCEPT_EULA=1
+
+Install-PackageProvider -Name NuGet -Force
+Install-Module PSWindowsUpdate -Confirm:$False -Force
+Import-Module PSWindowsUpdate
+Install-WindowsUpdate -AcceptAll -AutoReboot
